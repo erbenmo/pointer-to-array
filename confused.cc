a19c73d3d499209a0ae9ea51c764cbb2ce30849d
Erben Mo <moerben@amazon.com>
2013-11-02 13:36:48 -0700

    add output

--- confused.cc
+++ confused.cc
@@ -11,3 +11,15 @@ int main() {
   printf("*c: %d\n c: %p\n &c:%p\n", *c, c, &c);
   delete a;
 }
+
+/*
+ *a: 19
+  a: 0x7fbbd1c000e0
+ &a: 0x7fff5746aad8
+ *b: 19
+  b: 0x7fff5746aad4
+ &b: 0x7fff5746aad4
+ *c: 19
+  c: 0x7fff5746aad4
+ &c: 0x7fff5746aac8
+*/
